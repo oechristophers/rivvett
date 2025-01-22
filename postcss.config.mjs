5588d124705f8eababda2e9e6c046e7ce03fd6c5
oechristophers <oechristophers2020@gmail.com>
2025-01-22 22:45:40 +0100

    deployment fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-import': {},
+    'postcss-preset-env': {},
+    'postcss-nested': {},
+    'postcss-custom-properties': {},
+    'postcss-custom-selectors': {},
+    'postcss-import': {},
     autoprefixer: {},
     tailwindcss: {},
   },
