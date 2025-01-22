f64acd5ce65089c26a451c7613de46667f5c7178
oechristophers <oechristophers2020@gmail.com>
2025-01-22 12:06:35 +0100

    deployment fix

--- components/frontend/ProductGrid.jsx
+++ components/frontend/ProductGrid.jsx
@@ -1,20 +1,20 @@
-import styled from 'styled-components';
-import ProductCard from './ProductCard';
-import { useEffect, useState } from 'react';
+import styled from "styled-components";
+import ProductCard from "./ProductCard";
+import { useEffect, useState } from "react";
 
 const StyledGrid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr ;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
   gap: 10px;
   padding: 20px;
   @media screen and (max-width: 1030px) {
     grid-template-columns: ${({ page }) =>
-      page ? ' repeat(auto-fit, minmax(150px, 1fr));' : '1fr 1fr 1fr'};
+      page ? " repeat(auto-fit, minmax(150px, 1fr));" : "1fr 1fr 1fr"};
   }
 
   @media screen and (max-width: 768px) {
     grid-template-columns: ${({ page }) =>
-      page ? ' repeat(auto-fit, minmax(200px, 1fr));' : '1fr 1fr '};
+      page ? " repeat(auto-fit, minmax(200px, 1fr));" : "1fr 1fr "};
     padding: 0 28px;
   }
   @media screen and (max-width: 600px) {
@@ -29,22 +29,27 @@ const StyledCardContainer = styled.div`
 `;
 
 export default function ProductGrid({ products, page }) {
-  const [prevPath, setPrevPath] = useState('');
-  useEffect(()=>{
-    if(typeof window !== 'undefined' && localStorage){
-      const path = localStorage.getItem('prevPath');
+  const [prevPath, setPrevPath] = useState("");
+  useEffect(() => {
+    if (typeof window !== "undefined" && localStorage) {
+      const path = localStorage.getItem("prevPath");
       setPrevPath(path);
     }
-  })
+  });
   return (
     <StyledGrid page={page}>
       {products?.length > 1 &&
         products.map((product) => (
-          <StyledCardContainer>
+          <StyledCardContainer key={product._id}>
             <ProductCard
-              key={product._id}
               {...product}
-              genderName={product.gender.name !=='unisex'? product.gender.name : prevPath? prevPath : 'women'}
+              genderName={
+                product.gender.name !== "unisex"
+                  ? product.gender.name
+                  : prevPath
+                    ? prevPath
+                    : "women"
+              }
             />
           </StyledCardContainer>
         ))}
