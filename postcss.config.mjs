c94720cb68541fab067b88c4d0d018685f81c0ef
oechristophers <oechristophers2020@gmail.com>
2025-01-22 02:07:54 +0100

    deployment fixes--vercel why??

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-import': {},
+    'postcss-nested': {},
+    'postcss-preset-env': {
+      stage: 1,
+    },
     tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
