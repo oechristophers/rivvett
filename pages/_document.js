7aa76a5a7151fecc11b2e705b00355182001bd2b
oechristophers <oechristophers2020@gmail.com>
2025-01-23 07:12:31 +0100

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <meta name="theme-color" content="#222" /> 
+      <style>{`
+          html{
+            background-color: #222; 
+            overscroll-behavior: contain; 
+          }
+        `}</style>
+      </Head>
       <body>
         <Main />
         <NextScript />
